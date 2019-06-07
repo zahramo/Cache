6c5ad7637ad32a2c1d8b71256db6e1bd110e26d0
Zahra Moosavi <moosavizahra67@yahoo.com>
2019-06-07 13:36:27 +0430

    cacheCU aaded and dataMemory updated

---
+++ cacheCU.v
@@ -0,0 +1,41 @@
+`timescale 1ps/1ps
+module cacheCU(
+    clk,
+    rst,
+    hit,
+
+    cRead,
+    cWrite,
+    rRead,
+    rWrite,
+    selOut
+);
+input clk, rst, hit;
+output reg cRead, cwrite, rRead, rWrite, selOut;
+
+always @(posedge clk, posedge rst)begin
+    if(rst) begin
+        cRead = 1'b0;
+        cWrite = 1'b0;
+        rRead = 1'b0;
+        rWrite = 1'b0;
+    end
+    else begin
+        if(hit) begin 
+            cRead = 1'b1;
+            cWrite = 1'b0;
+            rRead = 1'b0;
+            rWrite = 1'b0;
+            selOut = 1'b1;
+        end
+        else begin
+            cRead = 1'b0;
+            cWrite = 1'b1;
+            rRead = 1'b1;
+            rWrite = 1'b0;
+            selOut = 1'b0;
+        end
+    end
+end
+
+endmodule
--- dataMemory.v
+++ dataMemory.v
@@ -1,19 +1,27 @@
 `timescale 1ps/1ps
 module DataMemory (
+    clk,
     address,
-    writeData,
-    readData,
+    address0,
+    address1,
+    address2,
+    address3,
     memWrite,
     memRead,
-    clk
+    
+    writeData,
+    dataOut,
+    block
     );
-    parameter WORD = 32, LENGTH = 4000, ADDRESSL = 32;
-    input [ADDRESSL-1:0]address;
+
+    parameter WORD = 32, LENGTH = 32768, ADDRESSL = 15, BLOCKSIZE = 4;
+    input [ADDRESSL-1:0]address, address0, address1, address2, address3;
     input [WORD-1:0]writeData;
     input memWrite,
     memRead,
     clk;
-    output  [WORD-1:0]readData;
+    output [WORD-1:0]dataOut;
+    output [WORD-1:0]block[BLOCKSIZE-1:0]
     reg [WORD-1:0]memory[LENGTH-1:0];
 
     integer i;
@@ -24,7 +32,7 @@ module DataMemory (
             memory[i] = 0;
         end
         for (i = 0; i < 2*LENGTH; i = i + 1) begin
-            memory[allData[i][31:0]] = allData[i+1];
+            memory[allData[i][WORD-1:0]] = allData[i+1];
             i=i+1;
         end
     end
@@ -33,6 +41,10 @@ module DataMemory (
         if (memWrite)
             memory[address] <= writeData;
     end
-    assign readData = memRead ? memory[address] : readData;
+    assign dataOut = memRead ? memory[address] : {WORD*{1'bz}};
+    assign block[0] = memRead ? mamory[address0] : {WORD*{1'bz}};
+    assign block[1] = memRead ? mamory[address1] : {WORD*{1'bz}};
+    assign block[2] = memRead ? mamory[address2] : {WORD*{1'bz}};
+    assign block[3] = memRead ? mamory[address3] : {WORD*{1'bz}};
                       
 endmodule

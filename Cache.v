19d752734b308526de72aaa404a579f8cfdf4b5a
ghminaei <ghminaei1037@gmail.com>
2019-06-07 14:07:36 +0430

    Cache updated

--- Cache.v
+++ Cache.v
@@ -25,27 +25,42 @@ module Cache (
     reg [WORD-1:0]cachMemory[LENGTH-1:0];
     reg [TAG-1:0]tagMemory[LENGTH-1:0];
     reg [VALID-1:0]validMemory[LENGTH-1:0];
+    reg hits;
     wire [TAG-1:0]tagIn;
     wire [ADDRESSL-1:0]adrInCache
     integer i;
     always @(posedge clk, posedge rst) begin
         if (rst) begin
+            hits = 0;
             for (i = 0; i < LENGTH; i = i + 1)
-                validMemory[i] = {WORD*{1'b0}};
+                validMemory[i] = 1'b0;
         end
         else begin
             if (cWrite) begin
-                cachMemory[adr0] = dataOutCache[0];
-                cachMemory[adr1] = dataOutCache[1];
-                cachMemory[adr2] = dataOutCache[2];
-                cachMemory[adr3] = dataOutCache[3];
+                cachMemory[adr0[ADDRESSL-1:0]] = dataOutCache[0];
+                tagMemory[adr0[ADDRESSL-1:0]] = adr0[ADDRESSL+TAG-1:ADDRESSL+TAG-4]
+                validMemory[adr0[ADDRESSL-1:0]] = 1'b1;
+
+                cachMemory[adr1[ADDRESSL-1:0]] = dataOutCache[1];
+                tagMemory[adr1[ADDRESSL-1:0]] = adr1[ADDRESSL+TAG-1:ADDRESSL+TAG-4]
+                validMemory[adr1[ADDRESSL-1:0]] = 1'b1;
+
+                cachMemory[adr2[ADDRESSL-1:0]] = dataOutCache[2];
+                tagMemory[adr2[ADDRESSL-1:0]] = adr2[ADDRESSL+TAG-1:ADDRESSL+TAG-4]
+                validMemory[adr2[ADDRESSL-1:0]] = 1'b1;
+
+                cachMemory[adr3[ADDRESSL-1:0]] = dataOutCache[3];
+                tagMemory[adr3[ADDRESSL-1:0]] = adr3[ADDRESSL+TAG-1:ADDRESSL+TAG-4]
+                validMemory[adr3[ADDRESSL-1:0]] = 1'b1;
             end
+            if (hit)
+                hits = hits + 1;
         end
     end
 
-    assign tagIn = address[ADDRESSL-1:ADDRESSL-4];
+    assign tagIn = address[ADDRESSL+TAG-1:ADDRESSL+TAG-4];
     assign adrInCache = address[ADDRESSL-1:0];
-    assign hit = validMemory[address] && tagIn == tagMemory[address] ? 1 : 0;
-    assign dataOutCache = cRead ? cachMemory[address] : {WORD*{1'bz}};
+    assign hit = validMemory[adrInCache] && tagIn == tagMemory[adrInCache] ? 1 : 0;
+    assign dataOutCache = cRead ? cachMemory[adrInCache] : {WORD*{1'bz}};
                       
 endmodule
